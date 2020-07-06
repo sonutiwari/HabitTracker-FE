427ee988699206edb41bb36461a94d892302f3cb
Sonu Tiwari <ankorha@gmail.com>
2020-07-06 22:29:25 +0530

    Added cards for habits

--- src/components/Habit/Habit.js
+++ src/components/Habit/Habit.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Habit extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( <div>
-            
-        </div> );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <div className="card" style={{width: '18rem'}}>
+          <div className="card-body">
+    <h5 className="card-title">{this.props.name}</h5>
+            <a href="#" className="btn btn-primary">
+              Mark it Done
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Habit;
